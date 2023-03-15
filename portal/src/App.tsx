127385d0c82f3888c7ee860d61bfe18b7af627cb
Matthew Tramantano <matt.tramantano@gmail.com>
2023-03-15 01:26:22 -0600

    added cors filter, created quick test

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const fetchCall = () => {
+  const id = "85a9d667-5cc0-4a3a-a722-24616993943e?85a9d667-5cc0-4a3a-a722-24616993943e";
+  fetch(`http://localhost:8080/scryfall/cards/cards/${id}`).then((response) => response.text()).then((body) => console.log(body))
+}
+
 function App() {
   return (
     <div>
       MTG DeckBuilder Portal
+      <button onClick={() => fetchCall()}>click me</button>
     </div>
   )
 };
