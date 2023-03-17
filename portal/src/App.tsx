4fe3ee1d302d715f3b80bd64456b1f7204e8d01b
Matthew Tramantano <matt.tramantano@gmail.com>
2023-03-17 13:02:08 -0600

    updated rwd, added cards to fetch component

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import AppBoundary from './AppBoundary';
 import CardViewer from './components/CardViewer/CardViewer';
 import Box from './components/RWD/Box';
 import { theme } from './components/RWD/constants';
 import Container from './components/RWD/Container';
 
-const fetchCall = () => {
-  const id = "85a9d667-5cc0-4a3a-a722-24616993943e?85a9d667-5cc0-4a3a-a722-24616993943e";
-  fetch(`http://localhost:8080/scryfall/cards/cards/${id}`).then((response) => response.text()).then((body) => console.log(body))
-}
+// const fetchCall = () => {
+//   const id = "85a9d667-5cc0-4a3a-a722-24616993943e?85a9d667-5cc0-4a3a-a722-24616993943e";
+//   fetch(`http://localhost:8080/scryfall/cards/cards`).then((response) => response.text()).then((body) => console.log(body))
+// }
 
 function App() {
   return (
     <div>
       MTG DeckBuilder Portal
-      <button onClick={() => fetchCall()}>click me</button>
+      {/* <button onClick={() => fetchCall()}>click me</button> */}
       <ThemeProvider theme={theme}>
-      <Container>
+      {/* <Container>
         <Box>Box 1</Box>
-        <Box>Box 2</Box>
-        <Box>Box 3</Box>
-      </Container>
+      </Container> */}
+      <AppBoundary>
+        <div>
+          <CardViewer />
+        </div>
+      </AppBoundary>
     </ThemeProvider>
     </div>
   )
--- portal/src/components/CardViewer/CardViewer.tsx
+++ portal/src/components/CardViewer/CardViewer.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import GridContainer, { getGridProps } from '../RWD/GridContainer';
 
 interface Props {}
 
 const CardViewer: React.FC<Props> = (props: Props) => {
+    const [ cards, setCards ] = useState<string[]>([]);
+    useEffect(() => {
+        const fetchCall = () => {
+            fetch(`http://localhost:8080/scryfall/cards/cards`)
+                .then((response) => response.text())
+                .then((body) => {
+                    const data = JSON.parse(body)
+                    setCards(data.data.splice(0, 5).map((card: { image_uris: {small: string, normal: string}; }) => card.image_uris.normal))
+                })
+          }
+        fetchCall();
+    }, [])
     return (
-        <div>
-            TEST
-        </div>
+        <GridContainer desktop={getGridProps(5)}>
+            {cards.map((card) => <span><img src={card} alt="card" /></span>)}
+        </GridContainer>
     );
 }
 
