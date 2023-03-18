8354e1c7d13f2cb49232d240021e3bd4b5ee825a
Matthew Tramantano <matt.tramantano@gmail.com>
2023-03-18 17:23:51 -0600

    improved placement of elements. Allowed them to be more flexibale relative to the card object

--- portal/src/components/CardViewer/CardDetails/CardDetails.tsx
+++ portal/src/components/CardViewer/CardDetails/CardDetails.tsx
@@ -2,40 +2,79 @@ import React from 'react'
 import styled from 'styled-components'
 import { Card } from '../types'
 import { CardImage } from './sub'
-import TextRow from './sub/TextRow';
+import TextRow from './sub/TextRow'
 
 interface Props {
-  card?: Card;
+  card?: Card
 }
 
 const Wrapper = styled.div`
-  box-sizing: border-box;
+  /* box-sizing: border-box; */
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   gap: 20px;
   padding: 10px;
 `
-const Columns = styled.div`
-  display: inline;
+
+const Description = styled.div`
+  position: relative;
+  height: 100%;
+`
+
+const TextBox = styled.div`
+  background-color: #8e8e8f;
+  color: white;
+  padding: 10px 5px;
+  position: absolute;
+  border: 1px solid black;
+  width: 400px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  font-size: 24px;
+`
+
+const Name = styled(TextBox)`
+  top: 10px;
+`
+const ManaCost = styled(TextBox)`
+  top: 74px;
+`
+
+const CardType = styled(TextBox)`
+  top: 274px;
+`
+
+const Keywords = styled(TextBox)`
+  top: 335px;
+`
+
+const OracleText = styled(TextBox)`
+  font-size: 20px;
+  top: 400px;
 `
 
 const CardDetails: React.FC<Props> = (props: Props) => {
-  const { card } = props;
+  const { card } = props
   return (
     <Wrapper>
-      <div>Card Container</div>
       <div>
-        <TextRow>{card?.name}</TextRow>
-        <TextRow>
+        <img src={card?.image_uris.normal} />
+      </div>
+      <Description>
+        <Name>{card?.name}</Name>
+        <ManaCost>
           <span>{card?.mana_cost}</span>
           <span>{card?.cmc}</span>
-        </TextRow>
-        <TextRow>{card?.type_line}</TextRow>
-        <TextRow>{card?.oracle_text}</TextRow>
-        <TextRow>{card?.keywords}</TextRow>
-      </div>
+        </ManaCost>
+        <Keywords>{card?.keywords}</Keywords>
+        <CardType>{card?.type_line}</CardType>
+        <OracleText>
+          {card?.oracle_text.split('\n').map((line) => (
+            <div>{line}</div>
+          ))}
+        </OracleText>
+      </Description>
     </Wrapper>
   )
 }
 
-export default CardDetails;
+export default CardDetails
--- portal/src/components/CardViewer/CardDetails/sub/TextRow.tsx
+++ portal/src/components/CardViewer/CardDetails/sub/TextRow.tsx
@@ -1,13 +1,12 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const TextRow = styled.div`
-    background-color: #8E8E8F;
-    color: white;
-    padding: 5px;
-    border: solid black #3c3c3c;
-    margin-bottom: 10px;
-    width: 350px;
-`;
+  background-color: #8e8e8f;
+  color: white;
+  padding: 5px;
+  /* border: solid black #3c3c3c; */
+  margin-bottom: 10px;
+  width: 350px;
+`
 
-
-export default TextRow;
+export default TextRow
